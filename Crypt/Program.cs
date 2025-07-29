ed7657a72cd41ebe5ed08bc2d7839e7c7bf13dba
JSandusky <j.sandusky@hotmail.com>
2025-07-29 19:38:13 -0400

    Initial commit

---
+++ Crypt/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Crypt
+{
+    class ConsoleColorPop : IDisposable
+    {
+        ConsoleColor col;
+        ConsoleColor old;
+        public ConsoleColorPop(ConsoleColor col)
+        {
+            this.col = col;
+            old = Console.ForegroundColor;
+            Console.ForegroundColor = col;
+        }
+
+        public void Dispose()
+        {
+            Console.ForegroundColor = old;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                using (new ConsoleColorPop(ConsoleColor.Green))
+                    Console.WriteLine("Crypt - Copryright (C) Jonathan Sandusky 2016");
+                Console.WriteLine(" ");
+                Console.WriteLine("    Usage:");
+                Console.WriteLine("crypt.exe <encrypt-mode> <path> <garble-names> -u [ungarble input path]");
+                Console.WriteLine("   encrypt-mode:");
+                Console.WriteLine("       /e = encrypt");
+                Console.WriteLine("       /d = decrypt");
+                Console.WriteLine("   path:");
+                Console.WriteLine("       either a dir or file path");
+                Console.WriteLine("       If path is a directory will recursively encrypt/decrypt files");
+                Console.WriteLine("   garble-names:");
+                Console.WriteLine("       default = off");
+                Console.WriteLine("       garble = garble file and directory names");
+                return;
+            }
+            string file = args[1];
+            bool encrypt = true;
+            string arg0 = args[0].ToLowerInvariant();
+            if (arg0.Equals("encrypt") || arg0.Equals("e") || arg0.Equals("/e") || arg0.Equals("-e"))
+                encrypt = true;
+            else if (arg0.Equals("decrypt") || arg0.Equals("d") || args[0].ToLowerInvariant().Equals("/d") || arg0.Equals("-d"))
+                encrypt = false;
+
+            bool garble = false;
+            if (args.Length >= 3)
+            {
+                if (args[2].ToLowerInvariant().Equals("garble"))
+                    garble  = true;
+            }
+
+            if (!encrypt && args.Length >= 4 && args[3].ToLowerInvariant().Equals("-u"))
+                file = SimplerAES.GarbleName(file);
+
+
+            using (new ConsoleColorPop(ConsoleColor.Green))
+            {
+                if (encrypt)
+                    Console.WriteLine("Encrypting...");
+                else
+                    Console.WriteLine("Decrypting...");
+            }
+
+            if (System.IO.Directory.Exists(file))
+            {
+                processDir(file, encrypt, garble);
+                if (garble)
+                    garbleDirs(file, encrypt);
+            }
+            else if (System.IO.File.Exists(file))
+                processFile(file, encrypt, garble);
+            else
+            {
+                using (new ConsoleColorPop(ConsoleColor.Red))
+                    Console.WriteLine("Error: unable to determine file/path");
+                return;
+            }
+            using (new ConsoleColorPop(ConsoleColor.Green))
+                Console.WriteLine(string.Format("{0} Complete", encrypt ? "Encryption":"Decryption"));
+        }
+
+        static void processDir(string path, bool encrypt, bool garble)
+        {
+            using (new ConsoleColorPop(ConsoleColor.Yellow))
+                Console.WriteLine(string.Format("Processing dir: {0}", path));
+            foreach (string str in System.IO.Directory.EnumerateFiles(path))
+                processFile(str, encrypt, garble);
+
+
+            foreach (string dir in System.IO.Directory.EnumerateDirectories(path))
+                processDir(dir, encrypt, garble);
+        }
+
+        static void garbleDirs(string path, bool encrypt)
+        {
+            foreach (string dir in System.IO.Directory.EnumerateDirectories(path))
+                garbleDirs(dir, encrypt);
+            System.IO.DirectoryInfo parent = System.IO.Directory.GetParent(path);
+            System.IO.DirectoryInfo current = new System.IO.DirectoryInfo(path);
+            if (encrypt)
+                System.IO.Directory.Move(path, System.IO.Path.Combine(parent.FullName, SimplerAES.GarbleName(current.Name)));
+            else
+                System.IO.Directory.Move(path, System.IO.Path.Combine(parent.FullName, SimplerAES.UngarbleName(current.Name)));
+        }
+
+        static void processFile(string file, bool encrypt, bool garble)
+        {
+            using (new ConsoleColorPop(ConsoleColor.Cyan))
+                Console.WriteLine(string.Format("{0} file '{1}'", encrypt ? "Encrypting" : "Decrypting", file));
+
+            if (encrypt)
+                SimplerAES.EncryptFile(file, garble);
+            else
+                SimplerAES.DecryptFile(file, garble);
+        }
+    }
+}
---
+++ Crypt/SimplerAES.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypt
+{
+    public class SimplerAES
+    {
+        private static string abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_";
+        private static byte[] key = { 123, 217, 19, 11, 24, 26, 85, 45, 114, 184, 27, 162, 37, 112, 222, 209, 241, 24, 175, 144, 173, 53, 196, 29, 24, 26, 17, 218, 131, 236, 53, 209 };
+
+        // Unused since the SHA1 is being used
+        private static byte[] vector = { 146, 64, 191, 111, 23, 3, 113, 119, 231, 121, 221, 112, 79, 32, 114, 156 };
+        private ICryptoTransform encryptor, decryptor;
+        private ASCIIEncoding encoder;
+
+        public static int Sha1Length = 20;
+
+        //public SimplerAES()
+        //{
+        //    RijndaelManaged rm = new RijndaelManaged();
+        //    encryptor = rm.CreateEncryptor(key, vector);
+        //    decryptor = rm.CreateDecryptor(key, vector);
+        //    encoder = new UTF8Encoding();
+        //}
+
+        public SimplerAES(byte[] sha1)
+        {
+            RijndaelManaged rm = new RijndaelManaged();
+            encryptor = rm.CreateEncryptor(key, sha1.Take(vector.Length).ToArray());
+            decryptor = rm.CreateDecryptor(key, sha1.Take(vector.Length).ToArray());
+            encoder = new ASCIIEncoding();
+        }
+
+        static int WrapZero(int x, int min, int max)
+        {
+            return ((x - min) % (max - min)) + min;
+        }
+
+        static char GarbleChar(char value, int index)
+        {
+            if (value == ' ')
+                return value;
+            int c = abc.IndexOf(value);
+            c += key[WrapZero(index, 0, key.Length - 1)];
+            return abc[WrapZero(c, 0, abc.Length - 1)];
+        }
+
+        static char UngarbleChar(char value, int index)
+        {
+            if (value == ' ')
+                return value;
+            int c = abc.IndexOf(value);
+            c -= key[WrapZero(index, 0, key.Length - 1)];
+            while (c < 0)
+                c += abc.Length - 1;
+            return abc[WrapZero(c, 0, abc.Length - 1)];
+        }
+
+        public static string GarbleName(string inName)
+        {
+            StringBuilder ret = new StringBuilder();
+            for (int i = 0; i < inName.Length; ++i)
+                ret.Append(GarbleChar(inName[i], i));
+            return ret.ToString();
+        }
+
+        public static string UngarbleName(string inName)
+        {
+            StringBuilder ret = new StringBuilder();
+            for (int i = 0; i < inName.Length; ++i)
+                ret.Append(UngarbleChar(inName[i], i));
+            return ret.ToString();
+        }
+
+        /// <summary>
+        /// Quickly encrypt a file at the given path
+        /// </summary>
+        /// <param name="path">file to encrypt</param>
+        public static void EncryptFile(string path, bool garble)
+        {
+            // Compute SHA1 for using as IV
+            byte[] data = System.IO.File.ReadAllBytes(path);
+            byte[] sha1 = SimplerAES.SHA1(data);
+            SimplerAES aes = new SimplerAES(sha1);
+
+            // Write SHA1 followed by encrypted file data
+            List<byte> ret = new List<byte>(sha1);
+            ret.AddRange(aes.Encrypt(data));
+
+            if (!garble)
+                System.IO.File.WriteAllBytes(path, ret.ToArray());
+            else
+            {
+                System.IO.File.WriteAllBytes(
+                    System.IO.Path.Combine(
+                        System.IO.Path.GetDirectoryName(path), GarbleName(System.IO.Path.GetFileNameWithoutExtension(path))) + System.IO.Path.GetExtension(path), ret.ToArray());
+                System.IO.File.Delete(path);
+            }
+        }
+
+        /// <summary>
+        /// Quickly decrypt a file at the given path
+        /// </summary>
+        /// <param name="path">path to file to decrypt</param>
+        public static void DecryptFile(string path, bool garble)
+        {
+            byte[] data = System.IO.File.ReadAllBytes(path);
+            // Extract the prepended SHA1 for IV
+            byte[] sha1 = data.Take(Sha1Length).ToArray();
+            SimplerAES aes = new SimplerAES(sha1);
+
+            // Decrypt the file data
+            if (!garble)
+                System.IO.File.WriteAllBytes(path, aes.Decrypt(data.Skip(Sha1Length).ToArray()));
+            else
+            {
+                System.IO.File.WriteAllBytes(
+                    System.IO.Path.Combine(
+                        System.IO.Path.GetDirectoryName(path), UngarbleName(System.IO.Path.GetFileNameWithoutExtension(path))) + System.IO.Path.GetExtension(path), data.Skip(Sha1Length).ToArray());
+                System.IO.File.Delete(path);
+            }
+        }
+
+        public static byte[] SHA1(byte[] buffer)
+        {
+            using (SHA1CryptoServiceProvider prov = new SHA1CryptoServiceProvider())
+                return prov.ComputeHash(buffer);
+        }
+
+        public string Encrypt(string unencrypted)
+        {
+            return Convert.ToBase64String(Encrypt(encoder.GetBytes(unencrypted)));
+        }
+
+        public string Decrypt(string encrypted)
+        {
+            return encoder.GetString(Decrypt(Convert.FromBase64String(encrypted)));
+        }
+
+        public byte[] Encrypt(byte[] buffer)
+        {
+            return Transform(buffer, encryptor);
+        }
+
+        public byte[] Decrypt(byte[] buffer)
+        {
+            return Transform(buffer, decryptor);
+        }
+
+        protected byte[] Transform(byte[] buffer, ICryptoTransform transform)
+        {
+            MemoryStream stream = new MemoryStream();
+            using (CryptoStream cs = new CryptoStream(stream, transform, CryptoStreamMode.Write))
+            {
+                cs.Write(buffer, 0, buffer.Length);
+            }
+            return stream.ToArray();
+        }
+    }
+}
